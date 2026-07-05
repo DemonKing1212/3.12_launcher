fc749f58cd447f4131e173d935a4f344746ed011
Ghosuto <clash.raja10@gmail.com>
2026-07-05 14:31:49 +0000

    Launcher3: Themed icon customization [1/2]
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- src/com/android/launcher3/settings/preferences/ColorPreference.java
+++ src/com/android/launcher3/settings/preferences/ColorPreference.java
@@ -52,6 +52,15 @@ public class ColorPreference extends Preference {
         this(context, null);
     }
 
+    public void setValue(int color) {
+        mColor = color;
+        updatePreview(mPreviewView, mColor);
+    }
+
+    public int getValue() {
+        return mColor;
+    }
+
     @Override
     public void onBindViewHolder(PreferenceViewHolder holder) {
         super.onBindViewHolder(holder);
@@ -216,6 +225,9 @@ public class ColorPreference extends Preference {
             .setPositiveButton(android.R.string.ok, (dialog, which) -> {
                 mColor = currentColor[0];
                 persistInt(mColor);
+                if (getOnPreferenceChangeListener() != null) {
+                    getOnPreferenceChangeListener().onPreferenceChange(this, mColor);
+                }
                 updatePreview(mPreviewView, mColor);
             })
             .setNegativeButton(android.R.string.cancel, null)
